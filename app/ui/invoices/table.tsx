4668fcacc15b4ab8e9d86a13d8faffcbaf827bef
sumzulfikar <sumaiya.zulfikar29@gmail.com>
2024-10-16 22:22:26 +0100

    Chapter 11 code

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { UpdateInvoice, DeleteInvoice } from '@/app/dashboard/invoices/buttons';
+import { UpdateInvoice, DeleteInvoice } from '@/app/ui/invoices/buttons';
 import InvoiceStatus from '@/app/dashboard/invoices/status';
 import { formatDateToLocal, formatCurrency } from '@/app/lib/utils';
 import { fetchFilteredInvoices } from '@/app/lib/data';
